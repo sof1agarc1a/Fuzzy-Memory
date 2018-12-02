e825bbf5f6f8ade18e60c079c504da7411f18fa7
Sofia Garcia <sofgar0614@gmail.com>
2018-12-03 00:23:35 +0100

    created function to flip cards, and to see if cards match, fixed bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const cards = [
+const cardsDiv = [
   { img: 'images/spongebob.jpg', nr: 1 },
   { img: 'images/squidward.jpg', nr: 2 },
   { img: 'images/patrick.jpg', nr: 3 },
@@ -19,19 +19,66 @@ const cards = [
   { img: 'images/sandy.png', nr: 8 },
 ];
 
-// cards.sort(function() {
-//   return Math.random() - Math.random()
-// });
-
 const container = document.querySelector('.container');
-
-for(let card of cards) {
-  const createCard = (img, datanumber) => {
+for(let cardDiv of cardsDiv) {
+  const createCard = (img, nr) => {
     return `
-    <div class="cards" data-card="${datanumber}">
+    <div class="cards" data-number="${nr}">
       <img class="front-card" src="images/frontCard.png" alt="error">
       <img class="hidden-card" src="${img}" alt="error">
     </div>`;
   };
-  container.innerHTML += (createCard(card.img, card.nr));
+  container.innerHTML += (createCard(cardDiv.img, cardDiv.nr));
 };
+
+const cards = document.querySelectorAll('.cards')
+
+let clicked = false;
+let firstClick, secondClick;
+let disableClickCard = false;
+
+function clickCard() {
+  if(disableClickCard) return;
+  if(this === firstClick) return;
+  this.classList.add('card-click');
+
+
+  if(clicked === false) {
+    clicked = true;
+    firstClick = this;
+
+  } else {
+    secondClick = this;
+    checkCardMatch();
+  }
+};
+
+function checkCardMatch() {
+  if(firstClick.dataset.number != secondClick.dataset.number) {
+    disableClickCard = true;
+    setTimeout(() => {
+    firstClick.classList.remove('card-click');
+    secondClick.classList.remove('card-click');
+    resetClick();
+  }, 900);
+  } else {
+    firstClick.removeEventListener('click', clickCard);
+    secondClick.removeEventListener('click', clickCard);
+    resetClick();
+  }
+};
+
+function resetClick() {
+  [clicked, disableClickCard] = [false, false];
+  [firstClick, secondClick] = [null, null];
+}
+
+
+cards.forEach(card => card.addEventListener('click', clickCard));
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomOrder = Math.floor(Math.random() * 16);
+    card.style.order = randomOrder;
+  });
+})();
