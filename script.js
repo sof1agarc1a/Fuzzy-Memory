d45b1f9082f1c4642b7223406d612c5825258aae
Sofia Garcia <sofgar0614@gmail.com>
2018-12-05 01:04:59 +0100

    fixed animations, fixed shuffle on replay

--- script.js
+++ script.js
@@ -19,32 +19,55 @@ const cardsDiv = [
   { img: 'images/sandy.png', nr: 8 },
 ];
 
-let shuffle = cardsDiv.sort(() => Math.random() - Math.random());
 
-const container = document.querySelector('.container');
-
-for(let cardDiv of cardsDiv) {
-  const createCard = (img, nr) => {
-    return `
-    <div class="display cards" data-number="${nr}">
-      <img class="front-card" src="images/frontCard.png" alt="error">
-      <img class="hidden-card" src="${img}" alt="error">
-    </div>`;
-  };
-  container.innerHTML += (createCard(cardDiv.img, cardDiv.nr));
-};
+const startGame = document.querySelector('.button')
 
 
-const startGame = document.querySelector('.start-game')
-
-const hideCards = document.querySelectorAll('.cards')
 
 startGame.addEventListener('click', () => {
   const minutes = document.querySelector(".minutes")
   const seconds = document.querySelector(".seconds")
-  startGame.classList.add('display');
+  const startGameNoDisplay = document.querySelector('.start-game')
 
-  hideCards.forEach(hiddenCard => hiddenCard.classList.remove('display'));
+  startGame.classList.add('display');
+  startGameNoDisplay.classList.add('display');
+  const container = document.querySelector('.container');
+
+  let points = 0;
+
+
+  function addAnimation() {
+    let newCards = document.querySelectorAll(".cards");
+    if(newCards[0]) {
+      newCards[0].className = "replay-animation";
+      setTimeout(addAnimation, 60);
+    }
+  }
+  setTimeout(addAnimation, 400);
+
+  setTimeout(() => {
+    let replaceAll = document.querySelectorAll(".replay-animation");
+    replaceAll.forEach(replace => replace.classList.add('cards'));
+    replaceAll.forEach(replace => replace.classList.remove('replay-animation'));
+  }, 2000);
+
+
+
+  const shuffleCards = cardsDiv.sort(() => {
+      return Math.random() - Math.random();
+  });
+  shuffleCards.forEach(cardDiv => {
+      const createCard = (img, nr) => {
+        return `
+        <div class="cards display" data-number="${nr}">
+        <img class="front-card" src="images/frontCard.png" alt="error">
+        <img class="hidden-card" src="${img}" alt="error">
+        </div>`;
+      };
+      container.innerHTML += (createCard(cardDiv.img, cardDiv.nr));
+  });
+  const hideCards = document.querySelectorAll('.cards')
+
 
   let count = 0;
   const timer = () => {
@@ -53,99 +76,65 @@ startGame.addEventListener('click', () => {
     seconds.innerHTML = (count%60).toString().padStart(2, "0");
   }
   const getTime = setInterval(timer, 1000);
-});
 
-const cards = document.querySelectorAll('.cards');
+  const cards = document.querySelectorAll('.cards');
+  cards.forEach(card => card.addEventListener('click', clickCard));
 
-cards.forEach(card => card.addEventListener('click', clickCard));
+  let clicked = false;
+  let firstClick, secondClick;
+  let disableClickCard = false;
 
-let clicked = false;
-let firstClick, secondClick;
-let disableClickCard = false;
+  function clickCard() {
+    if(disableClickCard) return;
+    if(this === firstClick) return;
+    this.classList.add('card-click');
 
-function clickCard() {
-  if(disableClickCard) return;
-  if(this === firstClick) return;
-  this.classList.add('card-click');
+    if(clicked === false) {
+      clicked = true;
+      firstClick = this;
 
-  if(clicked === false) {
-    clicked = true;
-    firstClick = this;
-
-  } else {
-    secondClick = this;
-    checkCardMatch();
-  }
-};
-
-let points = 0;
-
-function checkCardMatch() {
-  if(firstClick.dataset.number != secondClick.dataset.number) {
-    disableClickCard = true;
-    setTimeout(() => {
-    firstClick.classList.remove('card-click');
-    secondClick.classList.remove('card-click');
-    resetClick();
-  }, 900);
-  } else {
-    resetClick();
-    points++;
-
-    if(points === 1) {
-      function addClass() {
-        let winwin = document.querySelectorAll(".cards");
-        if(winwin[0]) {
-          winwin[0].className = "winwin";
-          setTimeout(addClass, 70);
-        }
-      }
-      setTimeout(addClass, 1000);
-
-          const replayButton = document.querySelector('.replay-button')
-          const replayGame = document.querySelector('.replay')
-
-          setTimeout(() => {
-            let winDisplayNone = document.querySelectorAll(".winwin");
-            winDisplayNone.forEach(replace => replace.classList.add('display'));
-
-            replayGame.classList.remove('display');
-            replayButton.classList.remove('display');
-          }, 3400);
-
-          replayButton.addEventListener('click', () => {
-
-
-
-            replayGame.classList.add('display');
-            points = 0;
-
-              function addAnimation() {
-                let winwin = document.querySelectorAll(".winwin");
-                if(winwin[0]) {
-                  winwin[0].className = "replay-animation";
-                  setTimeout(addAnimation, 70);
-                }
-              }
-              setTimeout(addAnimation, 10);
-
-              setTimeout(() => {
-                let replaceAll = document.querySelectorAll(".replay-animation");
-                replaceAll.forEach(replace => replace.classList.add('cards'));
-                replaceAll.forEach(replace => replace.classList.remove('replay-animation'));
-
-                // shuffle.sort(() => Math.random() - Math.random());
-
-              }, 2200);
-
-
-
-        });
-      }
+    } else {
+      secondClick = this;
+      checkCardMatch();
     }
   };
 
-function resetClick() {
-  [clicked, disableClickCard] = [false, false];
-  [firstClick, secondClick] = [null, null];
-}
+  function checkCardMatch() {
+    if(firstClick.dataset.number != secondClick.dataset.number) {
+      disableClickCard = true;
+      setTimeout(() => {
+        firstClick.classList.remove('card-click');
+        secondClick.classList.remove('card-click');
+        resetClick();
+    }, 900);
+    } else {
+      resetClick();
+      points++;
+      if(points === 1) {
+        function addClass() {
+          let winwin = document.querySelectorAll(".cards");
+          if(winwin[0]) {
+            winwin[0].className = "winwin";
+            setTimeout(addClass, 70);
+          }
+        }
+        setTimeout(addClass, 1300);
+        const startGame = document.querySelector('.button')
+        const startGameNoDisplay = document.querySelector('.start-game')
+        startGame.textContent = "Play again!!!!!";
+        setTimeout(() => {
+          let winDisplayNone = document.querySelectorAll(".winwin");
+          winDisplayNone.forEach(replace => replace.classList.add('display'));
+          startGame.classList.remove('display');
+          startGameNoDisplay.classList.remove('display');
+          container.innerHTML = "";
+        }, 6000);
+
+        }
+      }
+    };
+  function resetClick() {
+    [clicked, disableClickCard] = [false, false];
+    [firstClick, secondClick] = [null, null];
+  }
+});
